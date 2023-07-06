17861b94ce8e59d09ad3913a0ea27012283c64d3
Emirhan Eryılmaz <emirhan_eryilmaz@hotmail.com>
2023-07-06 14:28:51 +0300

    new game

--- Connect4/Connect4/connect4.cpp
+++ Connect4/Connect4/connect4.cpp
@@ -413,3 +413,134 @@ void RecordMoveToFile(const string& filename, const string& move)
 		cout << "Could not open the file." << endl;
 	}
 }
+
+void StartNewGame(playerInfo playerOne, playerInfo playerTwo, char board[10][10], int trueWidth, int trueLength, int dropChoice, int win, int full, int again, int dropLocation, int dropRow, std::string nameParam, std::string moveParam) {
+
+	cout << "First player please enter your name: ";
+
+	cin >> playerOne.playerName;
+
+	playerOne.playerID = 'X';
+
+	cout << "Second player please enter your name: ";
+
+	cin >> playerTwo.playerName;
+
+	playerTwo.playerID = 'O';
+
+	full = 0;
+
+	win = 0;
+
+	again = 0;
+
+	DisplayBoard(board);
+
+	do
+
+	{
+
+		dropLocation = PlayerDrop(board, playerOne);
+		dropRow = dropLocation / 10;
+		dropChoice = dropLocation % 10;
+		nameParam = playerOne.playerName + string("->");
+		moveParam = to_string(dropRow) + to_string(dropChoice);
+
+
+		CheckBelow(board, playerOne, dropChoice);
+		DisplayBoard(board);
+		RecordMoveToFile("hamle.txt", nameParam + moveParam);
+
+		win = CheckFour(board, playerOne);
+
+		if (win == 1)
+		{
+			PlayerWin(playerOne);
+			break;
+		}
+
+		dropLocation = PlayerDrop(board, playerTwo);
+		dropRow = dropLocation / 10;
+		dropChoice = dropLocation % 10;
+		nameParam = playerTwo.playerName + string("->");
+		moveParam = to_string(dropRow) + to_string(dropChoice);
+
+		CheckBelow(board, playerTwo, dropChoice);
+		DisplayBoard(board);
+		RecordMoveToFile("hamle.txt", nameParam + moveParam);
+
+
+		win = CheckFour(board, playerTwo);
+
+		if (win == 1)
+
+		{
+			PlayerWin(playerTwo);
+			break;
+		}
+
+
+		full = FullBoard(board);
+
+
+		if (full == 9)
+
+		{
+
+			cout << "Board full, draw!" << endl;
+			break;
+
+		}
+
+		// Option to stop the game
+
+		cout << "Press the 'ESC' key to stop the game. Press another key to continue: " << endl;
+
+
+		while (true) {
+			if (_kbhit()) {  // Check if any key has been pressed on the keyboard
+				char key = _getch();  // Get pressed key
+				if (key == 27) {  // Check ESC key
+					cout << "The game has been paused!" << endl;
+
+
+					cout << "Press 2 to restart the game. Press another key to exit: " << endl;
+
+					char key2 = _getch();  // Get pressed key
+
+					if (key2 == 50)
+
+					{
+						// Oyunu yeniden baþlatma iþlemleri
+						for (int i = 1; i <= 9; i++)
+						{
+							for (int ix = 1; ix <= 9; ix++)
+							{
+								board[i][ix] = '*';
+							}
+						}
+
+						main(); // restart the game by calling main function again
+					}
+
+					else
+
+					{
+						cout << "Goodbye!" << endl;
+						exit(0);
+						break;
+					}
+
+				}
+				else
+					break;
+
+			}
+
+		}
+
+
+	} while (true);
+
+
+}
