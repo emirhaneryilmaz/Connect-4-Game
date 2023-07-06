6296e0735b9dc192baaf923da4c3b652ca1830df
Emirhan Eryılmaz <emirhan_eryilmaz@hotmail.com>
2023-07-06 14:18:10 +0300

    main logic

--- Connect4/Connect4/connect4.cpp
+++ Connect4/Connect4/connect4.cpp
@@ -37,5 +37,49 @@ void StartNewGame(playerInfo playerOne, playerInfo playerTwo, char board[10][10]
 void ContinueLastGame(playerInfo playerOne, playerInfo playerTwo, char board[10][10], int dropChoice, int win, int full, int again, int dropLocation, int dropRow, std::string nameParam, std::string moveParam);
 
 int main() {
-	cout << "hello";
+	playerInfo playerOne{};
+	playerInfo playerTwo{};
+
+	char board[10][10]; // I defined the matrix as 10x10 because I don't want to enter 0 to the user and not to get confused while writing the code.
+
+	int trueWidth = 9;
+	int trueLength = 9;
+	int dropChoice{}, win{}, full{}, again{}, dropLocation{}, dropRow{};
+	string nameParam{}, moveParam{};
+
+
+
+	cout << "Lets Play Connect 4 !" << endl << endl;
+	cout << "1. Start a new game." << endl;
+	cout << "2. Load last game." << endl;
+	cout << "Make your choice: ";
+
+
+	int choice;
+	cin >> choice;
+
+
+	if (choice == 1) {
+		ofstream file("hamle.txt", ios::out | ios::trunc);
+		file.close();
+		StartNewGame(playerOne, playerTwo, board, trueWidth, trueLength, dropChoice, win, full, again, dropLocation, dropRow, nameParam, moveParam);
+
+	}
+
+	else if (choice == 2) {
+		ReadBoardFromFile(board);
+		ContinueLastGame(playerOne, playerTwo, board, dropChoice, win, full, again, dropLocation, dropRow, nameParam, moveParam);
+
+
+
+	}
+
+	else {
+
+		cout << "Invalid choice. The game is ending." << endl;
+		return 0;
+
+	}
+
+	return 0;
 }
